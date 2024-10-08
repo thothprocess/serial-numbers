a7f51a5ef8c86ec6c4fafaeece50f802fe277b1d
Quincy Glenn <the_ibis_head@thothprocess.com>
2024-10-08 02:30:44 -0400

    created initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Serial Numbers
+
+
+Manage serial numbers in an easy to use interface
---
+++ serial-numbers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Serial Numbers
+ * Description: Manage serial numbers in an easy to use interface
+ * Version: 0.1.0
+ * Author: ThothProcess
+ * Author URI: https://thothprocess.com
+ */
+
+
+if (!defined('ABSPATH')) {
+    exit();
+}
